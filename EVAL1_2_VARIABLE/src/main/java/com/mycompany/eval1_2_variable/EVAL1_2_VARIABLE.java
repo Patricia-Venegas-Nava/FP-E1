36f450406377111ae57e28fcc710644fde16b41d
Patricia-Venegas-Nava <L24550245@chihuahua2.tecnm.mx>
2024-08-24 14:49:51 -0600

    Add files via upload

---
+++ EVAL1_2_VARIABLE/src/main/java/com/mycompany/eval1_2_variable/EVAL1_2_VARIABLE.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval1_2_variable;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVAL1_2_VARIABLE {
+
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Tipo de datos
+        System.out.println("Hola Mundo"); //Cadena de texto
+        System.out.println(100);  //Entero
+        System.out.println(3.1416); //Númrto con punto flotante
+        System.out.println('c');  //Caracter
+        System.out.println(true);  //Valores lógicos (booleanos)
+        System.out.println(false);
+    
+    //;-->Fin de la instruccion
+    //ENTEROS
+    //int
+    //long
+    //short
+    
+     //NUMEROS CON PARTE DECIMAL(FLOTANTES)
+    //float
+    //double
+    
+    //CADENAS DE TEXTO
+    //string
+ 
+    //CARACTERES
+    //char
+   
+    //VALORES LOGICOS
+    //booleanos
+    
+    
+    //DECLACIÓN DE VAREABLES
+    //SINTAXIS PARA DECLARAR VAREABLES (REGLAS)
+    //TIPO_DE_DATO NOMBRE_DE_LA_VAREABLE;
+        
+      
+    String nombre;
+    String apellido;
+    String CURP;
+    float promedio;
+    
+}
+}

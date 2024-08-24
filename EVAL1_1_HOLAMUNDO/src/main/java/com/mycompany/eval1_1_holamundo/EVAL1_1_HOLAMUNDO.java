ee41894e561c2bfbf6cfa399c2a407b45f500eeb
Patricia-Venegas-Nava <L24550245@chihuahua2.tecnm.mx>
2024-08-24 14:49:14 -0600

    Add files via upload

---
+++ EVAL1_1_HOLAMUNDO/src/main/java/com/mycompany/eval1_1_holamundo/EVAL1_1_HOLAMUNDO.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.eval1_1_holamundo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVAL1_1_HOLAMUNDO {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
